68a3eda37a94a3b89d3331315b17e902b5cd4322
ShreeyaVarma <115877409+ShreeyaVarma@users.noreply.github.com>
2024-08-15 12:41:20 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
+
