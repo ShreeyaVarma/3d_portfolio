4ab0136b439ee69a0bf1e6c6436718482abe37c4
ShreeyaVarma <115877409+ShreeyaVarma@users.noreply.github.com>
2024-08-15 18:16:22 +0400

    srcc files

---
+++ srcc/assets/index.js
@@ -0,0 +1,201 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.hack_the_burgh_species = exports.kiosk = exports.cache = exports.sudoku = exports.bank = exports.solar = exports.chaos = exports.inf = exports.hyped = exports.uoe = exports.radicalai = exports.threejs = exports.typescript = exports.tailwind = exports.reactjs = exports.nodejs = exports.mips = exports.latex = exports.mysql = exports.java = exports.cpp = exports.c = exports.haskell = exports.python = exports.javascript = exports.html = exports.git = exports.figma = exports.docker = exports.css = exports.close = exports.menu = exports.github = exports.web = exports.mobile = exports.creator = exports.backend = exports.logo = undefined;
+
+var _logo = require("./logo.svg");
+
+var _logo2 = _interopRequireDefault(_logo);
+
+var _backend = require("./backend.png");
+
+var _backend2 = _interopRequireDefault(_backend);
+
+var _creator = require("./creator.png");
+
+var _creator2 = _interopRequireDefault(_creator);
+
+var _mobile = require("./mobile.png");
+
+var _mobile2 = _interopRequireDefault(_mobile);
+
+var _web = require("./web.png");
+
+var _web2 = _interopRequireDefault(_web);
+
+var _github = require("./github.png");
+
+var _github2 = _interopRequireDefault(_github);
+
+var _menu = require("./menu.svg");
+
+var _menu2 = _interopRequireDefault(_menu);
+
+var _close = require("./close.svg");
+
+var _close2 = _interopRequireDefault(_close);
+
+var _css = require("./tech/css.png");
+
+var _css2 = _interopRequireDefault(_css);
+
+var _docker = require("./tech/docker.png");
+
+var _docker2 = _interopRequireDefault(_docker);
+
+var _figma = require("./tech/figma.png");
+
+var _figma2 = _interopRequireDefault(_figma);
+
+var _git = require("./tech/git.png");
+
+var _git2 = _interopRequireDefault(_git);
+
+var _html = require("./tech/html.png");
+
+var _html2 = _interopRequireDefault(_html);
+
+var _javascript = require("./tech/javascript.png");
+
+var _javascript2 = _interopRequireDefault(_javascript);
+
+var _python = require("./tech/python.png");
+
+var _python2 = _interopRequireDefault(_python);
+
+var _haskell = require("./tech/haskell.png");
+
+var _haskell2 = _interopRequireDefault(_haskell);
+
+var _c = require("./tech/c.png");
+
+var _c2 = _interopRequireDefault(_c);
+
+var _cpp = require("./tech/cpp.png");
+
+var _cpp2 = _interopRequireDefault(_cpp);
+
+var _java = require("./tech/java.png");
+
+var _java2 = _interopRequireDefault(_java);
+
+var _mysql = require("./tech/mysql.png");
+
+var _mysql2 = _interopRequireDefault(_mysql);
+
+var _latex = require("./tech/latex.png");
+
+var _latex2 = _interopRequireDefault(_latex);
+
+var _mips = require("./tech/mips.png");
+
+var _mips2 = _interopRequireDefault(_mips);
+
+var _nodejs = require("./tech/nodejs.png");
+
+var _nodejs2 = _interopRequireDefault(_nodejs);
+
+var _reactjs = require("./tech/reactjs.png");
+
+var _reactjs2 = _interopRequireDefault(_reactjs);
+
+var _tailwind = require("./tech/tailwind.png");
+
+var _tailwind2 = _interopRequireDefault(_tailwind);
+
+var _typescript = require("./tech/typescript.png");
+
+var _typescript2 = _interopRequireDefault(_typescript);
+
+var _threejs = require("./tech/threejs.svg");
+
+var _threejs2 = _interopRequireDefault(_threejs);
+
+var _radicalai = require("./company/radicalai.jpeg");
+
+var _radicalai2 = _interopRequireDefault(_radicalai);
+
+var _uoe = require("./company/uoe.png");
+
+var _uoe2 = _interopRequireDefault(_uoe);
+
+var _hyped = require("./company/hyped.png");
+
+var _hyped2 = _interopRequireDefault(_hyped);
+
+var _inf = require("./company/inf.jpeg");
+
+var _inf2 = _interopRequireDefault(_inf);
+
+var _chaos = require("./projects/chaos.png");
+
+var _chaos2 = _interopRequireDefault(_chaos);
+
+var _solar = require("./projects/solar.png");
+
+var _solar2 = _interopRequireDefault(_solar);
+
+var _bank = require("./projects/bank.png");
+
+var _bank2 = _interopRequireDefault(_bank);
+
+var _sudoku = require("./projects/sudoku.jpeg");
+
+var _sudoku2 = _interopRequireDefault(_sudoku);
+
+var _cache = require("./projects/cache.png");
+
+var _cache2 = _interopRequireDefault(_cache);
+
+var _kiosk = require("./projects/kiosk.png");
+
+var _kiosk2 = _interopRequireDefault(_kiosk);
+
+var _hack_the_burgh_species = require("./projects/hack_the_burgh_species.jpeg");
+
+var _hack_the_burgh_species2 = _interopRequireDefault(_hack_the_burgh_species);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.logo = _logo2.default;
+exports.backend = _backend2.default;
+exports.creator = _creator2.default;
+exports.mobile = _mobile2.default;
+exports.web = _web2.default;
+exports.github = _github2.default;
+exports.menu = _menu2.default;
+exports.close = _close2.default;
+exports.css = _css2.default;
+exports.docker = _docker2.default;
+exports.figma = _figma2.default;
+exports.git = _git2.default;
+exports.html = _html2.default;
+exports.javascript = _javascript2.default;
+exports.python = _python2.default;
+exports.haskell = _haskell2.default;
+exports.c = _c2.default;
+exports.cpp = _cpp2.default;
+exports.java = _java2.default;
+exports.mysql = _mysql2.default;
+exports.latex = _latex2.default;
+exports.mips = _mips2.default;
+exports.nodejs = _nodejs2.default;
+exports.reactjs = _reactjs2.default;
+exports.tailwind = _tailwind2.default;
+exports.typescript = _typescript2.default;
+exports.threejs = _threejs2.default;
+exports.radicalai = _radicalai2.default;
+exports.uoe = _uoe2.default;
+exports.hyped = _hyped2.default;
+exports.inf = _inf2.default;
+exports.chaos = _chaos2.default;
+exports.solar = _solar2.default;
+exports.bank = _bank2.default;
+exports.sudoku = _sudoku2.default;
+exports.cache = _cache2.default;
+exports.kiosk = _kiosk2.default;
+exports.hack_the_burgh_species = _hack_the_burgh_species2.default;
+
+// import portfolio from "./projects/portfolio.png";
